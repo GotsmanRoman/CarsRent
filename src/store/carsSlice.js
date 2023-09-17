0a91fcb3c26cd94e5add806595290b2f10d66c2c
Gotsman Roman <gotsmanroman@gmail.com>
2023-09-18 02:03:13 +0300

    removed unused variables 2

--- src/store/carsSlice.js
+++ src/store/carsSlice.js
@@ -27,7 +27,6 @@ export const carsSlice = createSlice({
         filterByMileAgeTo,
       } = action.payload;
       state.filteredCars = [];
-      let totalItems = 0;
 
       state.filter = action.payload;
       state.filteredCars = state.cars
@@ -52,7 +51,6 @@ export const carsSlice = createSlice({
           if (filterByMileAgeTo && car.mileage > filterByMileAgeTo) {
             passFilter = false;
           }
-          totalItems++;
           return passFilter;
         })
         .slice(0, 8);
